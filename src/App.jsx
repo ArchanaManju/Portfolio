74fc3495f2439de23a0e7f963cc250176c65ddbd
ArchanaManju <archanahm2021@gmail.com>
2025-11-25 20:29:14 +0000

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import Projects from './components/Projects';
-import Certificates from './components/Certificates';
+
 import ExperienceSection from './components/ExperienceSection';
 import './index.css';
 import Header from './components/Header';
@@ -11,9 +9,9 @@ import ContactsCard from './components/ContactCard';
 
 function App() {
 return (
-<div className="min-h-screen bg-black text-slate-50">
+<div className="app-root">
 <Header />
-<main className="max-w-5xl mx-auto px-4 py-6 space-y-12">
+<main className="app-container">
 <About />
 <Certifications />
 <ExperienceSection />
@@ -23,4 +21,5 @@ return (
 </div>
 );
 }
+
 export default App;
